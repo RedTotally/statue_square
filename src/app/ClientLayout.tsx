74b9e801891260c7a7f0a0d30fb828319180ba0a
TheRed <94051573+RedTotally@users.noreply.github.com>
2024-09-17 23:52:52 +0800

    Update ClientLayout.tsx

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { getCookie, setCookie } from "cookies-next";
-
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  <></>
+  <>{children}</>
 }

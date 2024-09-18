00cfc0ba43ba92fe1c21801eb4fa240b59c2280e
TheRed <94051573+RedTotally@users.noreply.github.com>
2024-09-18 22:49:49 +0800

    Update content.tsx

--- src/app/product/content.tsx
+++ src/app/product/content.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { getCookie } from "cookies-next";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function Product() {
   let langCookie = getCookie("language");
